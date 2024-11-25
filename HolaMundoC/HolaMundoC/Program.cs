bb05054266fa96609a158a8fccacb166548ea8f3
Noemi Leon <noemi.telematik213@gmail.com>
2024-11-25 15:00:50 -0500

    Code sample

---
+++ HolaMundoC/HolaMundoC/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program 
+{
+    static void Main() 
+    {
+        string saludo = "Hola, Mundo!";
+        Console.WriteLine(saludo);
+        Console.WriteLine("Escribe tu nombre");
+        string nombre = Console.ReadLine();
+        Console.WriteLine($"Tu nombre es {nombre}");
+    }
+}
+
