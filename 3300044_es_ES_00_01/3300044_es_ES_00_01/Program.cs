5d94568f92444f6880abcf292eb0a5a64ee98b1e
Noemi Leon <noemi.telematik213@gmail.com>
2024-11-25 14:33:41 -0500

    Code sample

---
+++ 3300044_es_ES_00_01/3300044_es_ES_00_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using _3300044_es_ES_00_01;
+
+class Program
+{
+    static void Main()
+    {
+        // Crear instancias de Cancion
+        var cancion1 = new Cancion("Bohemian Rhapsody", "Queen", new TimeSpan(0, 5, 55));
+
+        // Crear instancia de ListaReproduccion
+        var playlist = new ListaReproduccion("Clásicos del Rock");
+
+        // Agregar canciones a ListaReproduccion
+        playlist.AgregarCancion(cancion1);
+
+        // Mostrar contenido de ListaReproduccion
+        playlist.MostrarCanciones();
+
+        // Reproducir una Cancion
+        cancion1.Reproducir();
+    }
+}
