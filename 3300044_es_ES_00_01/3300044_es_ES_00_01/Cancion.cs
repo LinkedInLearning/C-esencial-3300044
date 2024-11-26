11de6029f2a90e2e4768b861119d73c9d1d3c866
Noemi Leon <noemi.telematik213@gmail.com>
2024-11-26 13:16:08 -0500

    Code sample

---
+++ 3300044_es_ES_00_01/3300044_es_ES_00_01/Cancion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3300044_es_ES_00_01
+{
+    internal class Cancion
+    {
+        public string Titulo { get; set; } 
+        public string Artista { get; set; }
+        public TimeSpan Duracion { get; set; }
+
+        public Cancion(string titulo, string artista, TimeSpan duracion)
+        {
+            Titulo = titulo;
+            Artista = artista;
+            Duracion = duracion;
+        }
+
+        public async Task Reproducir()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine($"Reproduciendo '{Titulo}' de {Artista}... ({Duracion:mm\\:ss})"); //6.0 interpolación
+        }
+    }
+}
---
+++ 3300044_es_ES_00_01/3300044_es_ES_00_01/ListaReproduccion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3300044_es_ES_00_01
+{
+    internal class ListaReproduccion
+    {
+        public string Nombre { get; set; }
+        private List<Cancion>? canciones = new List<Cancion>(); //Tipos genéricos 2.0
+
+        public ListaReproduccion(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public void AgregarCancion(Cancion cancion)
+        {
+            canciones.Add(cancion);
+            Console.WriteLine($"Canción '{cancion.Titulo}' añadida a la playlist '{Nombre}'.");
+        }
+
+        #region 3.0
+        public void MostrarCanciones()
+        {
+            Console.WriteLine($"\nPlaylist: {Nombre}");
+            foreach (var cancion in canciones)
+            {
+                Console.WriteLine($"- {cancion.Titulo} de {cancion.Artista} ({cancion.Duracion:mm\\:ss})");
+            }
+        }
+
+        #endregion
+        public List<Cancion> ObtenerCancionPorTitulo(string titulo)
+        {
+            var cancionesConTitulo = canciones.Where(c => c.Titulo == titulo);
+            return cancionesConTitulo.ToList<Cancion>();
+        }
+    }
+}
