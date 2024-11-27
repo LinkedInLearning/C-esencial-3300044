17452b7b9c102315e327afb581d899ed4dec4309
Noemi Leon <noemi.telematik213@gmail.com>
2024-11-27 00:47:20 -0500

    Code sample - tipos numericos en practica

--- TiposEnCSharp/TiposEnCSharp/Program.cs
+++ TiposEnCSharp/TiposEnCSharp/Program.cs
@@ -6,15 +6,28 @@
         Console.WriteLine("Tipos numéricos:");
 
         int miInt = -100;
-
-
         byte miByte = 255;
-
         long miLong = 255;
-
         decimal miDecimal = 500.50M;
-
         float miFloat = 1f;
+
+        //Conversión cadenas a tipo numérico
+        Console.WriteLine("Ingrese un número entero");
+        string entrada = Console.ReadLine();
+        int num1 = int.Parse(entrada);
+        Console.WriteLine("Ingrese otro número entero");
+        string entrada2 = Console.ReadLine();
+        int num2 = int.Parse(entrada2);
+
+        //Operaciones y redondeo
+        int suma = num1 + num2;
+        Console.WriteLine($"Suma = {suma} ");
+
+        double division = suma / 3.0;
+        double redondeo = Math.Round(division, 2);
+
+        //Conversión entre tipos numéricos
+        byte sumaByte = (byte)suma;
         #endregion
     }
 }
