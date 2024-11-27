51f0881548b1b7b5f9e39e518d64965c21753937
Noemi Leon <noemi.telematik213@gmail.com>
2024-11-27 00:23:44 -0500

    Code sample - tipos numericos

---
+++ TiposEnCSharp/TiposEnCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Tipos numéricos
+        Console.WriteLine("Tipos numéricos:");
+
+        int miInt = -100;
+
+
+        byte miByte = 255;
+
+        long miLong = 255;
+
+        decimal miDecimal = 500.50M;
+
+        float miFloat = 1f;
+        #endregion
+    }
+}
