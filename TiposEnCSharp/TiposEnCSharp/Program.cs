5600af3f4127fa08f9f9ea54149ea139bc058ab1
Noemi Leon <noemi.telematik213@gmail.com>
2024-11-27 14:18:46 -0500

    Code sample - tuple

--- TiposEnCSharp/TiposEnCSharp/Program.cs
+++ TiposEnCSharp/TiposEnCSharp/Program.cs
@@ -2,20 +2,11 @@
 
 internal class Program
 {
-    enum Turno
-    {
-        Ninguno,
-        Matutino,
-        Vespertino,
-        Nocturno
-    }
-
-    enum Estado : byte { Activo = 1, Inactivo = 0 };
     private static void Main(string[] args)
     {
-        #region Tipo enum
+        #region Tipo tupla
 
-        Console.WriteLine($"{(Turno)3}");
+        //Console.WriteLine($"{}");
         #endregion
 
     }
