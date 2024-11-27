40f2d5ff892292a516715a0dcab8017668736095
Noemi Leon <noemi.telematik213@gmail.com>
2024-11-27 14:46:06 -0500

    Code sample - record

--- TiposEnCSharp/TiposEnCSharp/Program.cs
+++ TiposEnCSharp/TiposEnCSharp/Program.cs
@@ -2,11 +2,24 @@
 
 internal class Program
 {
+    public record Cancion (string Titulo, string Artista, TimeSpan Duracion);
     private static void Main(string[] args)
     {
-        #region Tipo tupla
-
+        #region Tipo record
+        var cancion = new Cancion("Bohemian Rhapsody", "Queen", new TimeSpan(0, 5, 55));
         //Console.WriteLine($"{}");
+
+        var cancion2 = cancion with { Duracion = new TimeSpan(0, 5, 58) };
+
+        var cancion3 = new Cancion("Bohemian Rhapsody", "Queen", new TimeSpan(0, 5, 55));
+        if (cancion == cancion3)
+        {
+            Console.WriteLine($"Son iguales!");
+        }
+
+        var (titulo, artista, tiempo) = cancion;
+        Console.WriteLine($"{tiempo}");
+
         #endregion
 
     }
