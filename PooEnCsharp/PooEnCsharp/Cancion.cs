fdb99dde322a7599dfeb78082b8d351efbc02d53
Noemi Leon <noemi.telematik213@gmail.com>
2024-11-29 13:36:41 -0500

    Code sample - poo

---
+++ PooEnCsharp/PooEnCsharp/Cancion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooEnCsharp
+{
+    public class Cancion
+    {
+        #region Propiedades
+        public string Titulo { get; set; }
+        public string Artista { get; set; }
+        public int Duracion { get; set; } // Duración en segundos
+        #endregion
+
+        #region Constructores
+        public Cancion(string titulo, string artista, int duracion)
+        {
+            Titulo = titulo;
+            Artista = artista;
+            Duracion = duracion;
+        }
+        #endregion
+
+        #region Métodos
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Título: {Titulo}, Artista: {Artista}, Duración: {Duracion} segundos");
+        }
+        #endregion
+    }
+}
---
+++ PooEnCsharp/PooEnCsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using PooEnCsharp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* POO:
+         * Herencia,
+         * Polimorfismo,
+         * Abstracción,
+         * Encapsulamiento
+         */
+
+
+        // Crear una instancia de Cancion
+        Cancion cancion = new Cancion("Imagine", "John Lennon", 183);
+
+        // Mostrar información de la canción
+        cancion.MostrarInformacion();
+    }
+}
