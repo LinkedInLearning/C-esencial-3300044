a9669481acd32fd76ade38983994535f974196e7
Noemi Leon <noemi.telematik213@gmail.com>
2024-11-29 14:15:56 -0500

    Code sample - poo3

--- PooEnCsharp/PooEnCsharp/Cancion.cs
+++ PooEnCsharp/PooEnCsharp/Cancion.cs
@@ -8,8 +8,25 @@ namespace PooEnCsharp
 {
     public class Cancion
     {
+        #region Campos
+        private string titulo;
+        #endregion
+
         #region Propiedades
-        public string Titulo { get; set; }
+        public string Titulo { 
+            get { return titulo; } 
+            set {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    titulo = value;
+                }
+                else
+                {
+                    throw new ArgumentException("El titulo no puede ser nulo o vacío");
+                }
+                
+            }
+        }
         public string Artista { get; set; }
         public int Duracion { get; set; } // Duración en segundos
         #endregion
--- PooEnCsharp/PooEnCsharp/Program.cs
+++ PooEnCsharp/PooEnCsharp/Program.cs
@@ -4,11 +4,18 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Instancia de Cancion
-        Cancion cancion = new Cancion("Imagine", "John Lennon", 183);
-        Cancion cancionFav = new Cancion("Crazy", "Aerosmith", 185);
+        try { 
+            // Instancia de Cancion
+            Cancion cancion = new Cancion("Imagine", "John Lennon", 183);
+            Cancion cancionFav = new Cancion("Crazy", "Aerosmith", 185);
+            Cancion cancion2 = new Cancion("", "Aerosmith", 185);
 
-        // Mostrar información de la canción
-        cancion.MostrarInformacion();
+            // Mostrar información de la canción
+            cancion.MostrarInformacion();
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
     }
 }
